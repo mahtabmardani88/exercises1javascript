ae12db0286e36176d8936ba44a4a8cf5d13736a9
mahtabmardani88 <128320457+mahtabmardani88@users.noreply.github.com>
2024-02-08 15:51:38 +0100

    javascript

---
+++ JS.js
@@ -0,0 +1,93 @@
+
+// ==========================================================================================
+// Exercise 1
+
+// let user={
+//     name : "John",
+//     years : 30
+// }
+// let{name, years:age,isAdmin=false}=user;
+// console.log(user);
+// console.log(name);
+// console.log(age);
+// console.log(isAdmin);
+
+
+// Exercise 2-------------------------------------
+
+// let ourPlanet="Earth";
+// let visitor = "mahtab";
+
+
+// Exercise 3 -------------------------------------
+
+// let phrase = "Hello"              this is a variable 
+
+// if (true) { if                    our condition is true :
+//   let user = "John";              set user john
+//   function sayHi() {              this is on way to join
+//     alert(`${phrase}, ${user}`)   say phrase and user
+//   }
+// }
+
+// sayHi();                          now we have this output hello john
+
+// Exercise 4--------------------------------------
+
+// let user={};
+// user.name ="john";
+// user.surname = "smith";
+// user.name ="pete";
+// console.log(user.name);
+
+// delete user.name;
+// console.log(user);
+
+// Exercise 5 --------------------------------------
+
+// const user ={
+//     name:"john"
+// }
+// user.name="pete"
+// console.log(user);  yes is it possible and this code work and cont is constant a variable and we can add or remove it
+
+// Exercise 6 -----------------------------------------
+// let salaries ={
+//     fred : 100,
+//     ted : 160,
+//     ghaith: 130
+// }
+// let sum =0;
+// for (let key in salaries) {
+//     if (salaries.lenght !== 0) {
+//         sum += salaries[key];
+//     }
+//     else{
+//         sum = 0;
+//     }
+// }
+// console.log(sum);
+
+// Exercise 7-------------------------------------------------
+// if (a + b < 4) {
+//     result = 'Below';
+//   } else {
+//     result = 'Over';
+//   }
+
+//   let sum = (a+b<4) ? "Bellow" : "over";
+
+// Exercise 8 ------------------------------------------------
+// let message;
+
+// if (login == 'Employee') {
+//   message = 'Hello';
+// } else if (login == 'Director') {
+//   message = 'Greetings';
+// } else if (login == '') {
+//   message = 'No login';
+// } else {
+//   message = '';
+// }
+// let massage =(login == 'Employee')? "hello" : (login == 'director') ? "greetings" : (login == '')? 'no log in':'';
+
